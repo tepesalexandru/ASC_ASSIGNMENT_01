ac88e7cd230730d266d867bcaa39781331835adf
Tepes Alexandru <godzplayer@hotmail.com>
2020-10-08 11:50:16 +0300

    Imbunatatiri

--- ASC_ASSIGNMENT_01/Program.cs
+++ ASC_ASSIGNMENT_01/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using System;
 using System.Globalization;
 
 namespace ASC_ASSIGNMENT_01
@@ -10,11 +11,23 @@ namespace ASC_ASSIGNMENT_01
             Console.WriteLine("Legea lui Moore indica faptul ca puterea de calcul se dubleaza la fiecare 18 luni.");
             Console.WriteLine("Acest program permite aflarea timpului ramas pana cand puterea de calcul va fi de n ori mai mare decat cea din prezent.");
             Console.Write("Introdu o valoare pentru n: ");
-            int n = int.Parse(Console.ReadLine());
-            int luni = Convert.ToInt32(Math.Log2(n)) * 18;
-            int ani = luni / 12;
-            luni = luni % 12;
-            Console.WriteLine($"Raspuns: Puterea de calcuc va fi de {n} ori mai mare in {ani} ani si {luni} luni.");
+            try
+            {
+                int n = int.Parse(Console.ReadLine());
+                int luni = Convert.ToInt32(Math.Log2(n)) * 18;
+                int ani = luni / 12;
+                luni = luni % 12;
+                Console.Write($"Raspuns: Puterea de calcuc va fi de {n} ori mai mare in {ani} ani si {luni} luni, ");
+
+                DateTime thisDay = DateTime.Now;
+                Console.WriteLine($"pe data de {thisDay.AddYears(ani).AddMonths(luni).ToString("dd/MM/yyyy")}.");
+
+            } catch (FormatException e)
+            {
+                Console.WriteLine("Va rog sa introduceti un numar natural.");
+            }
+
+
             Console.ReadKey();
         }
     }
