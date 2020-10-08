245d53f50284851209e0b34d3730033ea1cdc832
Tepes Alexandru <godzplayer@hotmail.com>
2020-10-08 11:33:13 +0300

    Rezolvarea problemei

--- ASC_ASSIGNMENT_01/Program.cs
+++ ASC_ASSIGNMENT_01/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace ASC_ASSIGNMENT_01
 {
@@ -6,7 +7,15 @@ namespace ASC_ASSIGNMENT_01
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Legea lui Moore indica faptul ca puterea de calcul se dubleaza la fiecare 18 luni.");
+            Console.WriteLine("Acest program permite aflarea timpului ramas pana cand puterea de calcul va fi de n ori mai mare decat cea din prezent.");
+            Console.Write("Introdu o valoare pentru n: ");
+            int n = int.Parse(Console.ReadLine());
+            int luni = Convert.ToInt32(Math.Log2(n)) * 18;
+            int ani = luni / 12;
+            luni = luni % 12;
+            Console.WriteLine($"Raspuns: Puterea de calcuc va fi de {n} ori mai mare in {ani} ani si {luni} luni.");
+            Console.ReadKey();
         }
     }
 }
